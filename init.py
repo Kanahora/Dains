d468b1dd7319fadb78be0a7735fa540812902a64
Kanahora <115061442+Kanahora@users.noreply.github.com>
2023-02-02 04:39:06 +1100

    Update init.py

--- init.py
+++ init.py
@@ -27,19 +27,10 @@ def show_register():
 
 @app.route('/settings', methods=['GET', 'POST'])
 def show_settings():
-	function = settings.account_update()
-	user_update = function[0]
-	user_status = function[1]
-
-	return render_template('andrew/settings.html', form=user_update, user_status=user_status)
-
+	return settings.account_update()
 
 @app.route('/account_delete', methods=['GET', 'POST'])
 def account_delete():
-	function = settings.account_delete()
+	return settings.account_delete()
 
-	if function == False:
-		return redirect(url_for('show_login'))
-	else:
-		return render_template('index.html', user_status=account_delete)
 app.run()
