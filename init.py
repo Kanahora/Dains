94f18cca6c825cfc9e92b281bcc1eb2e6ab398a0
rhaylenee <119665308+rhaylenee@users.noreply.github.com>
2023-02-06 16:22:37 +0800

    Added app routing to display reward_manage
    Just an html test

--- init.py
+++ init.py
@@ -5,12 +5,13 @@ from python.src.Insan import accounts
 app = Flask(__name__)
 app.secret_key = "secret"
 
-
+# Glenn
 @app.route("/", methods=["GET", "POST"])
 def show_index():
     return index.run()
+# END Glenn
 
-
+# Andrew
 @app.route('/login', methods=['GET', 'POST'])
 def show_login():
     return login.login()
@@ -34,11 +35,19 @@ def show_settings():
 @app.route('/account_delete', methods=['GET', 'POST'])
 def account_delete():
     return settings.account_delete()
+# END Andrew
 
-
+# Insan
 @app.route("/staff/accounts_manage", methods=['GET', 'POST'])
 def accounts_manage():
     return accounts.manage()
+# END Insan
+
+# Rhaylene
+@app.route("/staff/reward_manage", methods=['GET', 'POST'])
+def reward_manage():
+    return render_template("Rhaylene/reward_manage.html")
+# END Rhaylene
 
 
 app.run()
