05411efb3293ab4cb141085b1991d3a2ab6841f3
Kanahora <115061442+Kanahora@users.noreply.github.com>
2023-02-09 17:49:42 +1100

    Update glenn.py

--- python/src/Glenn/glenn.py
+++ python/src/Glenn/glenn.py
@@ -1,6 +1,6 @@
-from index import run_index
+from index import index
 from flask import *
 def run(app: Flask):
 	@app.route("/", methods=["GET", "POST"])
 	def show_index():
-		return run_index()
+		return index()
