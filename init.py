e6c70dffb2d59ae276322f2c79119798b6bd44a7
Kanahora <115061442+Kanahora@users.noreply.github.com>
2023-02-09 18:51:23 +1100

    Update init.py

--- init.py
+++ init.py
@@ -1,45 +1,21 @@
 from flask import *
-from python.src.Andrew import andrew
-from python.src.Glenn import glenn
-from python.src.Insan import insan
-from python.src.Rhaylene import rewards
-
 app = Flask(__name__)
 app.secret_key = "secret"
 
-glenn.run(app)
-andrew.run(app)
-insan.run(app)
+import python.src.Andrew.index as Andrew
+import python.src.Glenn.index as Glenn
+import python.src.Insan.index as Insan
+import python.src.Rhaylene.index as Rhaylene
 
-# Insan
-@app.route("/staff/accounts_manage", methods=['GET', 'POST'])
-def accounts_manage():
-    return accounts.manage()
-
-@app.route("/staff/accounts_update/<id>", methods=['GET', 'POST'])
-def accounts_update():
-    return accounts.update(id)
-
-@app.route('/staff/delete_user/<id>', methods=['POST'])
-def delete_user(id):
-    return accounts.delete_user(id)
-# END Insan
-
-# Rhaylene
-@app.route("/staff/rewards_manage", methods=['GET', 'POST'])
-def rewards_manage():
-    return rewards.reward_manage()
-
-@app.route("/staff/reward_create", methods=['GET', 'POST'])
-def create_reward():
-    return rewards.create_reward()
-# END Rhaylene
+Andrew.run(app)
+Glenn.run(app)
+Insan.run(app)
+Rhaylene.run(app)
 
 # Ryan
 @app.route("/staff/inventory_manage", methods=['GET', 'POST'])
 def inventory_manage():
     return render_template("Ryan/inventory_manage.html")
 # END Ryan
-
 
 app.run()
