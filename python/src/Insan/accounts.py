9f3726328c41d8aa7ad0af1db865918c4d22b4ee
Kanahora <115061442+Kanahora@users.noreply.github.com>
2023-02-09 18:36:13 +1100

    Update accounts.py

--- python/src/Insan/accounts.py
+++ python/src/Insan/accounts.py
@@ -2,7 +2,6 @@ from flask import *
 import shelve
 from python.classes.form import UpdateAccount
 
-
 def manage():
     dict = {}
     users = shelve.open("database/users")
@@ -21,40 +20,40 @@ def manage():
 
 
 def update(id):
-    forms = UpdateAccount(request.form)
-    if request.method == 'POST' and forms.validate():
-        user_shelf = shelve.open('database/users')
+    form = UpdateAccount(request.form)
+    if request.method == 'POST' and form.validate():
+        user_list = shelve.open('database/users')
 
-        users = user_shelf.get(id)
-        users.set_name(forms.name.data)
-        users.set_phone(forms.phone.data)
-        users.set_email(forms.email.data)
-        users.set_status(forms.status.data)
-        user_shelf[id] = users
-        user_shelf.close()
+        user = user_list.get(id)
+        user.set_name(form.name.data)
+        user.set_phone(form.phone.data)
+        user.set_email(form.email.data)
+        print(form.status.data)
+        if form.status.data == "CUSTOMER":
+            user.set_customer()
+        elif form.status.data == "STAFF":
+            user.set_staff()
+        elif form.status.data == "ADMIN":
+            user.set_admin()
+        user_list[id] = user
 
-        return redirect(url_for('accounts_manage'))
+        return redirect(url_for('accounts_manage', count=len(user_list), user_list=user_list))
     else:
         user_dict = {}
-        user_shelf = shelve.open('database/users')
-        user_dict = user_shelf
+        user_list = shelve.open('database/users')
+        user_dict = user_list
 
-        users = user_dict.get(id)
-        forms.name.data = users.get_name()
-        forms.phone.data = users.get_phone()
-        forms.email.data = users.get_email()
-        forms.status.data = users.get_status()
-        user_shelf.close()
-        return render_template('Insan/accounts_update.html', form=forms)
+        user = user_dict.get(id)
+        form.name.data = user.get_name()
+        form.phone.data = user.get_phone()
+        form.email.data = user.get_email()
+        form.status.data = user.get_status()
+        user_list.close()
+        return render_template('Insan/accounts_update.html', form=form)
 
 
 def delete_user(id):
-    user_dict = {}
-    user_shelf = shelve.open('database/users', 'w')
-    user_dict = user_shelf
+    user_list = shelve.open('database/users', 'w')
+    user_list.pop(id)
+    return redirect(url_for('accounts_manage', count=len(user_list), user_list=user_list))
 
-    user_dict.pop(id)
-    user_shelf = user_dict
-    user_shelf.close()
-
-    return redirect(url_for('accounts_manage'))
