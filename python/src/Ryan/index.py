edb114cdd138ac47b232a364d5e02f4ac10b013f
NovaIZ01 <119665263+NovaIZ01@users.noreply.github.com>
2023-02-11 21:28:10 +0800

    Update index.py

--- python/src/Ryan/index.py
+++ python/src/Ryan/index.py
@@ -1,5 +1,18 @@
 from flask import *
+from python.src.Ryan import manage,create,update,delete
 def run(app: Flask):
-	@app.route("/staff/inventory_manage")
+	@app.route("/staff/inventory_manage", methods=["GET", "POST"])
 	def show_inventory_manage():
-		return render_template("Ryan/inventory_manage.html")
+		return manage.inventory_manage()
+
+	@app.route("/staff/inventory_update/<product_id>", methods=["GET", "POST"])
+	def show_inventory_update():
+		return update.inventory_update()
+	
+	@app.route("/staff/inventory_create/", methods=["GET", "POST"])
+	def show_inventory_create():
+		return create.inventory_create()
+	
+	@app.route("/staff/delete_product/<product_id>", methods=["GET", "POST"])
+	def delete_product(id):
+		return delete.delete_product(id)
