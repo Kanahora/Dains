07953cd1d20330e141fcf0b3f0a76fce837bdd6e
rhaylenee <119665308+rhaylenee@users.noreply.github.com>
2023-02-12 22:05:19 +0800

    Update rewards.py

--- python/src/Rhaylene/rewards.py
+++ python/src/Rhaylene/rewards.py
@@ -1,46 +1,52 @@
 from flask import *
 from python.classes import Reward
 import shelve
-import python.classes.form as form
-
+import python.src.Rhaylene.form as form
+from python.classes import Identifier
 
 def reward_manage():
     rewards = shelve.open("database/rewards")
     return render_template('Rhaylene/rewards_manage.html', count=len(rewards), rewards_list=rewards)
 
 
-def update_reward(id):
-    update_reward_form = form.CreateRewardForm(request.form)
+def reward_update(id):
+    update_reward_form = form.UpdateRewardForm(request.form)
     rewards = shelve.open("database/rewards")
+    
     if request.method == 'POST' and update_reward_form.validate():
         rewards = shelve.open("database/rewards")
-        reward = rewards.get(id)
+        reward = rewards[id]
+        
         reward.set_reward_name(update_reward_form.name.data)
         reward.set_reward_points(update_reward_form.points.data)
-        reward.set_reward_description(update_reward_form.description.data)
+        reward.set_reward_discount(update_reward_form.discount.data)
 
         rewards[id] = reward
-        return redirect(url_for('rewards_manage', count=len(rewards), rewards_list=rewards))
+        return redirect(url_for('reward_manage', count=len(rewards), rewards_list=rewards))
     else:
-        reward = rewards.get(id)
+        reward = rewards[id]
+        
         update_reward_form.name.data = reward.get_reward_name()
         update_reward_form.points.data = reward.get_reward_points()
-        update_reward_form.description.data = reward.get_reward_description()
-        return redirect(url_for("rewards_update", form=update_reward_form))
-
+        update_reward_form.discount.data = reward.get_reward_discount()
+        return render_template('Rhaylene/rewards_update.html', form=update_reward_form)
+        
 
 def delete_reward(id):
     rewards = shelve.open('database/rewards', 'w')
+    
     rewards.pop(id)
-    return redirect(url_for('rewards_manage', count=len(rewards), rewards_list=rewards))
+    return redirect(url_for('reward_manage', count=len(rewards), rewards_list=rewards))
 
 
 def create_reward():
     create_reward_form = form.CreateRewardForm(request.form)
     if request.method == 'POST' and create_reward_form.validate():
         rewards = shelve.open("database/rewards")
-        reward = Reward.Reward(create_reward_form.name.data,
-                               create_reward_form.points.data, create_reward_form.description.data)
+        reward = Reward.Reward(Identifier.Identifier().new_reward_id(),
+                               create_reward_form.name.data,
+                               create_reward_form.points.data, 
+                               create_reward_form.discount.data)
         rewards[reward.get_reward_id()] = reward
-        return redirect(url_for('rewards_manage'))
+        return redirect(url_for('reward_manage'))
     return render_template('Rhaylene/rewards_create.html', form=create_reward_form)
