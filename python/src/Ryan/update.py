9eedba6ef541d60f7cb98abf0b2a7cb9a08f5c9d
NovaIZ01 <119665263+NovaIZ01@users.noreply.github.com>
2023-02-12 19:08:02 +0800

    Update update.py

--- python/src/Ryan/update.py
+++ python/src/Ryan/update.py
@@ -2,7 +2,7 @@ from flask import *
 import shelve
 import python.src.Ryan.Form as form  
 
-def inventory_update(id):
+def inventory_update(product):
     update_inventory = form.UpdateProduct(request.form)
     product = shelve.open("database/products")
     if request.method == 'POST' and update_inventory.validate():
@@ -13,14 +13,17 @@ def inventory_update(id):
         product.set_category(update_inventory.category.data)
         product.set_price(update_inventory.price.data)
         product.set_stock(update_inventory.stock.data)
+        product.set_addon(update_inventory.addon.data)
 
-        products[id] = product
-        return redirect(url_for('inventory_manage', count=len(products), products_list=products))
+        products[product] = product
+        return redirect(url_for('show_inventory_manage', count=len(products), products_list=products))
     else:
-        product = products.get(id)
+        product = products.get(product)
         update_inventory.product_id.data = product.get_id()
         update_inventory.product_name.data = product.get_name()
         update_inventory.category.data = product.get_category()
         update_inventory.price.data = product.get_price()
         update_inventory.stock.data = product.get_stock()
-        return redirect(url_for("inventory_update", form=update_inventory))
+        update_inventory.addon.data = product.can_addon()
+        return redirect(url_for("show_inventory_update", form=update_inventory))
+        return render_template('Ryan/inventory_update.html', form=form)
