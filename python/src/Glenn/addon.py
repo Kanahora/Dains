c4bb652e7d023d27656ff59b2ad8a4632d1d0037
Kanahora <115061442+Kanahora@users.noreply.github.com>
2023-02-13 08:26:13 +1100

    Delete python/src/Glenn directory

--- python/src/Glenn/addon.py
+++
@@ -1,6 +0,0 @@
-import shelve
-from flask import *
-from python.classes import Cart
-def run(id):
-	products = shelve.open("database/products")
-	return render_template("Glenn/addon.html", id=id, products=products, Cart=Cart)
