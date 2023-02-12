44833d8ea57270c77e585fe8e11dbe3b26a8255c
NovaIZ01 <119665263+NovaIZ01@users.noreply.github.com>
2023-02-12 18:53:41 +0800

    Delete python/src/Ryan directory

--- python/src/Ryan/delete.py
+++
@@ -1,8 +0,0 @@
-from flask import *
-from python.classes import Product
-import shelve
-
-def delete_reward(id):
-    products = shelve.open('database/products', 'w')
-    products.pop(id)
-    return redirect(url_for('inventory_manage', count=len(products), products_list=products))
--- python/src/Ryan/manage.py
+++
@@ -1,6 +0,0 @@
-from flask import *
-import shelve
-
-def inventory_manage():
-    inventory = shelve.open("database/products")
-    return render_template('Ryan/inventory_manage.html', count=len(inventory), product_list=inventory)
