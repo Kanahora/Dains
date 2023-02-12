220cc6d22574f0daedde5dfc927fd647a58f6bcf
NovaIZ01 <119665263+NovaIZ01@users.noreply.github.com>
2023-02-12 19:05:55 +0800

    Update delete.py

--- python/src/Ryan/delete.py
+++ python/src/Ryan/delete.py
@@ -2,7 +2,7 @@ from flask import *
 from python.classes import Product
 import shelve
 
-def delete_reward(id):
+def delete_product(id):
     products = shelve.open('database/products', 'w')
     products.pop(id)
-    return redirect(url_for('inventory_manage', count=len(products), products_list=products))
+    return redirect(url_for('show_inventory_manage', count=len(products), products_list=products))
