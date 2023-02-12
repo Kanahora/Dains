979c9f8f781051d0e6c54f5aef8e9ae3855246cb
rhaylenee <119665308+rhaylenee@users.noreply.github.com>
2023-02-12 22:04:48 +0800

    Update index.py

--- python/src/Rhaylene/index.py
+++ python/src/Rhaylene/index.py
@@ -1,10 +1,19 @@
 from python.src.Rhaylene import rewards
 from flask import *
 def run(app: Flask):
-	@app.route("/staff/rewards_manage", methods=["GET", "POST"])
-	def rewards_manage():
+	@app.route('/staff/rewards_manage', methods=['GET', 'POST'])
+	def reward_manage():
 		return rewards.reward_manage()
 
-	@app.route("/staff/reward_create", methods=["GET", "POST"])
+	@app.route('/staff/rewards_create', methods=['GET', 'POST'])
 	def create_reward():
 		return rewards.create_reward()
+
+	@app.route('/staff/rewards_update/<id>/', methods=['GET', 'POST'])
+	def reward_update(id):
+		return rewards.reward_update(id)
+
+	@app.route('/staff/deleteReward/<id>', methods=['POST'])
+	def delete_reward(id):
+		return rewards.delete_reward(id)
+	
