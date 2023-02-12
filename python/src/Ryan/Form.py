bdfb0b5bed508923375bf4a343999e8bbec4c61e
Kanahora <115061442+Kanahora@users.noreply.github.com>
2023-02-12 22:25:21 +1100

    Update Form.py

--- python/src/Ryan/Form.py
+++ python/src/Ryan/Form.py
@@ -3,12 +3,6 @@ from wtforms.fields import *
 from wtforms.validators import *
 import shelve
 #Done by Ryan
-def duplicate_product_id(form,product_id):
-    inventory_db = shelve.open(('database/products'))
-    for inventory in inventory_db:
-        if product_id.data == inventory_db[inventory].get_id():
-            raise ValidationError('The Product Id is already listed')
-#Done by Ryan
 def duplicate_product_name(form,product_name):
     inventory_db = shelve.open(('database/products'))
     for inventory in inventory_db:
@@ -16,9 +10,6 @@ def duplicate_product_name(form,product_name):
             raise ValidationError('The Product name is already listed')
 # Done by Ryan
 class CreateProduct(Form):
-    product_id = StringField("Product Id", validators = [DataRequired(message = 'Enter a Product Id'),
-                                                       
-                                                       duplicate_product_id])
     product_name = StringField("Product Name",
                                validators =[
                                    validators.Length(min=1, max=100, message = 'Enter a product name that is not more than 100 characters'),
@@ -41,9 +32,6 @@ class CreateProduct(Form):
     addon = BooleanField("Is Add On?",default = False )                                                 
 #Done by Ryan
 class UpdateProduct(Form):
-    product_id = IntegerField("Product Id", validators = [DataRequired(message = 'Enter a Product Id'),
-                                                       validators.NumberRange(min=1, max=100, message = 'Enter a number between 1-1000'),
-                                                       duplicate_product_id])
     product_name = StringField("Product Name",
                                validators =[
                                    validators.Length(min=1, max=100, message = 'Enter a product name that is not more than 100 characters'),
@@ -63,3 +51,4 @@ class UpdateProduct(Form):
                                                        validators.NumberRange(min=1, max=1000, message = 'Enter a number between 1-1000')])
 
     addon = BooleanField("Is Add On?",default = False )
+
