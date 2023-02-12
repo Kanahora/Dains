e86ec51da8b181335b16dd82eee67d7470d70c2a
Kanahora <115061442+Kanahora@users.noreply.github.com>
2023-02-13 02:58:54 +1100

    Update menu.py

--- python/src/Glenn/menu.py
+++ python/src/Glenn/menu.py
@@ -13,9 +13,11 @@ def run(category):
 		id = request.form.get("add-to-cart")
 		if id:
 			if session.get("name"):
+				#if products[id].can_addon():
+					#return redirect(url_for("show_addon", id=id, Cart=Cart, products=products))
+				#else:
 				Cart.Cart(session.get("id")).add_product(id)
 				return render_template("Glenn/menu.html", products=products, Cart=Cart, category=category, categories=categories)
 			else:
 				return redirect(url_for("show_login"))
-	
 	return render_template("Glenn/menu.html", category=category, Cart=Cart, products=products, categories=categories)
